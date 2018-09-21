0ba25f4257eb605899fb8504d0ccdf42f081a6a9
Amir Hedayati <a3hedaya@uwaterloo.ca>
2018-09-21 12:06:39 -0400

    Original commit for CharCounter 1.0

---
+++ README.md
@@ -0,0 +1,2 @@
+# CharCounter
+Unpacked chrome extension to count characters and words highlighted by user on any webpage.
---
+++ counter.js
@@ -0,0 +1,6 @@
+var highlight=window.getSelection().toString();
+var i =0;
+
+var seperated = highlight.split(" ");
+var count = seperated.length;
+alert("Word count: "+ count+"\n" +"Character count: "+highlight.length+"\n");
